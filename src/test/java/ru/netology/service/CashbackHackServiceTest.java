2345a99fd5d36eb20da21c529bcb0f6b758ff87a
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-03-19 21:20:17 +0300

    added annotations Junit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,40 +1,50 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
     @Test
-    void shouldCalculateRemainIfUnderBoundary() {
+    public void shouldCalculateRemainIfUnderBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 800;
 
         int actual = service.remain(amount);
         int expected = 200;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
     @Test
-    void shouldCalculateRemainIfOverBoundary() {
+    public void shouldCalculateRemainIfOverBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1800;
 
         int actual = service.remain(amount);
         int expected = 200;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
     @Test
-    void shouldCalculateRemainIfNotAmount() {
+    public void shouldCalculateRemainIfNotAmount() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
 
         int actual = service.remain(amount);
         int expected = 1000;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldCalculateRemainIfMultiplyAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
     }
 
 }
