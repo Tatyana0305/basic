c388fbb923a59691a874f57fe498520d9d70ec6c
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-03-19 20:31:45 +0300

    edited tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -36,14 +36,5 @@ class CashbackHackServiceTest {
 
         assertEquals(expected, actual, "wrong remain");
     }
-    @Test
-    void shouldCalculateRemainIfEqualsBoundary() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
 
-        int actual = service.remain(amount);
-        int expected = 0;
-
-        assertEquals(expected, actual, "wrong remain");
-    }
 }
