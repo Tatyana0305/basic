99015caf527d20a358dddb022365977cda9e0961
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-03-19 08:06:53 +0300

    added class and test

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldCalculateRemainIfUnderBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(expected, actual, "wrong remain");
+    }
+    @Test
+    void shouldCalculateRemainIfOverBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(expected, actual, "wrong remain");
+    }
+    @Test
+    void shouldCalculateRemainIfNotAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual, "wrong remain");
+    }
+    @Test
+    void shouldCalculateRemainIfEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual, "wrong remain");
+    }
+}
