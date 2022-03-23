a354e291020ea73f906ce8edfc5741047738d6da
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-03-23 05:19:19 +0300

    added failing tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -38,5 +38,25 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+    @Test
+    public void shouldCalculateRemainIfMultiplyAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldCalculateRemainIfMultiplyAmount2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
 
 }
